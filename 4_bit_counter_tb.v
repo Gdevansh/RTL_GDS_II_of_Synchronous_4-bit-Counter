94a223c62d0fdbccc3f6adcb15b19a939522b988
Devansh Gupta <102615388+Gdevansh@users.noreply.github.com>
2024-09-03 07:38:57 +0530

    4_bit_counter_tb.v

--- 4_bit_counter_tb.v
+++ 4_bit_counter_tb.v
@@ -1,1 +1,17 @@
+% Testbench %
 
+module tb_counter(); 
+reg clk,rst; 
+wire [3:0] out; 
+counter inst1(clk,rst,out); 
+initial begin 
+clk = 1'b0; 
+forever #5 clk = ~clk; 
+end
+initial begin 
+rst = 1'b1; 
+#50 rst = 1'b0; 
+#10 rst = 1'b1; 
+#1000 $finish; 
+end 
+endmodule 
